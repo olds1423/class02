38686860ade0bba0e8a8213773bbaa77042d8451
olds1423 <olds1423@gmail.com>
2016-05-25 21:01:38 -0700

    First try

---
+++ app.js
@@ -0,0 +1,9 @@
+// var userName = prompt("What's your name")
+// alert("Welcome to my page, " + userName);
+
+var questionOne = prompt("What state was I born in?").toLowerCase();
+if (questionOne === "washington") {
+alert("Yay, you know me os well! \n Gold Star!");
+} else {
+alert("Sorry, you dont know me...Sad face...");
+}
